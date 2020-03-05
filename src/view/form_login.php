3adf63cd2378c927bf12ccde3abfb07af36bfd07
slv-improver <k.moret.slv@gmail.com>
2020-03-05 12:13:27 +0100

    add connection fields

---
+++ src/view/form_login.php
@@ -0,0 +1,7 @@
+<form method="post" action="index.php?route=login">
+	<p><label for="login">Login</label>
+	<input type="text" id="login" name="login"></p>
+	<p><label for="password">Mot de passe</label>
+	<input type="password" id="password" name="password"></p>
+	<p><input type="submit" value="Connexion" name="submit"></p>
+</form>
---
+++ src/view/login.php
@@ -0,0 +1,5 @@
+<?php $this->title = "Connexion"; ?>
+
+<div>
+	<?php require_once 'form_login.php'; ?>
+</div>
