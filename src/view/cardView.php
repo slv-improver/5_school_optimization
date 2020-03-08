8b6e3ad1fba181c625bdf847d9e6db891f6ae04d
slv-improver <k.moret.slv@gmail.com>
2020-03-08 14:42:13 +0100

    add template of info section in cardView

--- src/view/cardView.php
+++ src/view/cardView.php
@@ -1,10 +1,32 @@
 <?php $title = "Fiche de l\'enfant"; ?>
 <?php
-$lastName = $child->getLastName();
-$firstName = $child->getFirstName();
+$lastName = strtoupper($child->getLastName());
+$firstName = ucfirst($child->getFirstName());
 $birthDate = $child->getBirthDate();
 $diff = $child->getBirthDate()->diff(new DateTime());
 ?>
 
-<p><?= $lastName ?> <?= $firstName ?> : né le <?= $birthDate->format('d/m/Y') ?><br>
-<?= $diff->y ?> ans <?= $diff->m ?> mois <?= $diff->d ?> jours</p>
+<h2 align='center'><?= $lastName ?> <?= $firstName ?></h2>
+<p>
+	né le <?= $birthDate->format('d/m/Y') ?><br>
+	<?= $diff->y ?> ans <?= $diff->m ?> mois <?= $diff->d ?> jours
+</p>
+
+<section>
+	<h2 align='center'>Informations Administratives</h2>
+
+	<p>Adresse : </p>
+
+	<p>Pere : </p>
+	<p>Mere : </p>
+
+	<h3>Personnes autorisees a recuperer l'enfant</h3>
+
+	<p>Nom : </p>
+	<p>Prenom : </p>
+	<p>N° CIN : </p>
+
+	<h3>Medical</h3>
+
+	<p>Allergies : </p>
+</section>
