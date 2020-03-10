08b9e4a3a2854df89fa9f12b6275cc48e6c4bb8b
slv-improver <k.moret.slv@gmail.com>
2020-03-10 11:08:42 +0100

    add link to return on home page

--- src/view/cardView.php
+++ src/view/cardView.php
@@ -7,5 +7,7 @@ $diff = $child->getBirthDate()->diff(new DateTime());
 $gender = $child->getGender() === 'm' ? '' : 'e';
 ?>
 
+<a href="index.php?route=listChildren">Accueil</a>
+
 <p><?= $lastName ?> <?= $firstName ?> : né<?= $gender ?> le <?= $birthDate->format('d/m/Y') ?><br>
 <?= $diff->y ?> ans <?= $diff->m ?> mois <?= $diff->d ?> jours</p>
