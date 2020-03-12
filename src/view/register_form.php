62b69804ed73ff0017e71978e3995f4b93ce1cbc
slv-improver <k.moret.slv@gmail.com>
2020-03-12 03:48:22 +0100

    add input radio for gender child

--- src/view/register_form.php
+++ src/view/register_form.php
@@ -1,6 +1,8 @@
 <form action="index.php?route=addChild" method="post">
 	<p><label for="name">Nom :</label><input type="text" name="last_name" id="name"></p>	
 	<p><label for="firstname">Prenom :</label><input type="text" name="first_name" id="firstname"></p>	
-	<p><label for="birth_date">Date de naissance :</label><input type="date" name="birth_date" id="birth_date"></p>	
+	<p><label for="birth_date">Date de naissance :</label><input type="date" name="birth_date" id="birth_date"></p>
+	<p><label for="gender">Garçon</label><input type="radio" name="gender" value="m"></p>
+	<p><label for="gender">Fille</label><input type="radio" name="gender" value="f"></p>
 	<input type="submit" value="Enregistrer" name="submit">
 </form>
