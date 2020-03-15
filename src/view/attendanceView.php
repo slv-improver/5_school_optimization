6dc7ef0b395c56bd6872142648189791a5dfdbf3
Algor <k.moret.slv@gmail.com>
2020-03-15 23:00:12 +0100

    move form tag outside td tags

--- src/view/attendanceView.php
+++ src/view/attendanceView.php
@@ -10,10 +10,11 @@
 	</tr>
 <?php foreach ($children as $child) : ?>
 	<tr>
+		<form action="index.php?route=attendance&amp;childId=<?= $child->getId() ?>" method="post">
 		<td><?= $child->getLastName() ?></td>
 		<td><?= $child->getFirstName() ?></td>
 		<td>
-			<form action="index.php?route=attendance&amp;childId<?= $child->getId() ?>" method="post">
+				<label for="val-1_<?= $child->getId() ?>" class="intd">Non renseigné<input type="radio" name="attendanceAmount" value="-1" id="val-1_<?= $child->getId() ?>" checked></label>
 				<label for="val-1<?= $child->getId() ?>" class="intd">Non renseigné<input type="radio" name="attendanceAmount<?= $child->getId() ?>" value="-1" id="val-1<?= $child->getId() ?>" checked></label>
 				<label for="val0<?= $child->getId() ?>" class="intd">Absent<input type="radio" name="attendanceAmount<?= $child->getId() ?>" value="0" id="val0<?= $child->getId() ?>"></label>
 				<label for="val05<?= $child->getId() ?>" class="intd">Mi-journée<input type="radio" name="attendanceAmount<?= $child->getId() ?>" value="0.5" id="val05<?= $child->getId() ?>"></label>
@@ -21,8 +22,9 @@
 		</td>
 		<td>
 			<input type="submit" name="submit" value="Enregistrer">
-			</form>
 		</td>
+		</td>
+		</form>
 	</tr>
 	<?php endforeach; ?>
 </table>
