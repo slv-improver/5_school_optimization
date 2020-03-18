6d63b2396510eec0ffb6a8f40a44cfed924b8846
Algor <k.moret.slv@gmail.com>
2020-03-18 18:54:27 +0100

    use childrenHaveAttendance in attendanceView
    to display them at the bottom of the table

--- src/view/attendanceView.php
+++ src/view/attendanceView.php
@@ -25,4 +25,21 @@
 			</form>
 		</tr>
 	<?php endforeach; ?>
+	<?php foreach ($childrenHaveAttendance as $child) : ?>
+		<tr>
+			<form action="index.php?route=attendance&amp;childId=<?= $child->getId() ?>" method="post">
+				<td><?= $child->getLastName() ?></td>
+				<td><?= $child->getFirstName() ?></td>
+				<td>
+					<label for="val-1_<?= $child->getId() ?>" class="intd">Non renseigné<input type="radio" name="attendanceAmount" value="-1" id="val-1_<?= $child->getId() ?>" checked></label>
+					<label for="val0_<?= $child->getId() ?>" class="intd">Absent<input type="radio" name="attendanceAmount" value="0" id="val0_<?= $child->getId() ?>"></label>
+					<label for="val05_<?= $child->getId() ?>" class="intd">Mi-journée<input type="radio" name="attendanceAmount" value="0.5" id="val05_<?= $child->getId() ?>"></label>
+					<label for="val1_<?= $child->getId() ?>" class="intd">Présent<input type="radio" name="attendanceAmount" value="1" id="val1_<?= $child->getId() ?>"></label>
+				</td>
+				<td>
+					<input type="submit" name="submit" value="Enregistrer" class="attendance">
+				</td>
+			</form>
+		</tr>
+	<?php endforeach; ?>
 </table>
