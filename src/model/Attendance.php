413e3eafa9d5f941de89f8e976a37eb7cca5c6c3
Algor <k.moret.slv@gmail.com>
2020-03-18 12:12:08 +0100

    add Attendance object with property day and amount

---
+++ src/model/Attendance.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\src\model;
+
+use DateTime;
+
+class Attendance
+{
+	private $amount = [];
+	private $day = [];
+
+	public function __construct(array $data)
+	{
+		foreach ($data as $row) {
+			$this->hydrate($row);
+		}
+	}
+	
+	public function hydrate(array $data)
+	{
+		foreach ($data as $key => $value) {
+			
+			$method = 'set' . ucfirst($key);
+			if (method_exists($this, $method)) {
+				$this->$method($value);
+			}
+		}
+	}
+	
+	public function setAmount($value)
+	{
+		$this->amount[] = $value;
+	}
+	public function getAmount()
+	{
+		return $this->amount;
+	}
+
+	public function setDay($value)
+	{
+		$this->day[] = new DateTime($value);
+	}
+	public function getDay()
+	{
+		return $this->day;
+	}
+}
