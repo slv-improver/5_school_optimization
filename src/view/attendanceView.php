56f89d3cd1b982f8a6cf9dda49a9be80c3a806c5
Algor <k.moret.slv@gmail.com>
2020-03-18 18:49:35 +0100

    format file by adding \t

--- src/view/attendanceView.php
+++ src/view/attendanceView.php
@@ -8,21 +8,21 @@
 		<th>Prenom</th>
 		<th>Présence</th>
 	</tr>
-<?php foreach ($children as $child) : ?>
-	<tr>
-		<form action="index.php?route=attendance&amp;childId=<?= $child->getId() ?>" method="post">
-		<td><?= $child->getLastName() ?></td>
-		<td><?= $child->getFirstName() ?></td>
-		<td>
-				<label for="val-1_<?= $child->getId() ?>" class="intd">Non renseigné<input type="radio" name="attendanceAmount" value="-1" id="val-1_<?= $child->getId() ?>" checked></label>
-				<label for="val0_<?= $child->getId() ?>" class="intd">Absent<input type="radio" name="attendanceAmount" value="0" id="val0_<?= $child->getId() ?>"></label>
-				<label for="val05_<?= $child->getId() ?>" class="intd">Mi-journée<input type="radio" name="attendanceAmount" value="0.5" id="val05_<?= $child->getId() ?>"></label>
-				<label for="val1_<?= $child->getId() ?>" class="intd">Présent<input type="radio" name="attendanceAmount" value="1" id="val1_<?= $child->getId() ?>"></label>
-		</td>
-		<td>
-			<input type="submit" name="submit" value="Enregistrer" class="attendance">
-		</td>
-		</form>
-	</tr>
+	<?php foreach ($children as $child) : ?>
+		<tr>
+			<form action="index.php?route=attendance&amp;childId=<?= $child->getId() ?>" method="post">
+				<td><?= $child->getLastName() ?></td>
+				<td><?= $child->getFirstName() ?></td>
+				<td>
+					<label for="val-1_<?= $child->getId() ?>" class="intd">Non renseigné<input type="radio" name="attendanceAmount" value="-1" id="val-1_<?= $child->getId() ?>" checked></label>
+					<label for="val0_<?= $child->getId() ?>" class="intd">Absent<input type="radio" name="attendanceAmount" value="0" id="val0_<?= $child->getId() ?>"></label>
+					<label for="val05_<?= $child->getId() ?>" class="intd">Mi-journée<input type="radio" name="attendanceAmount" value="0.5" id="val05_<?= $child->getId() ?>"></label>
+					<label for="val1_<?= $child->getId() ?>" class="intd">Présent<input type="radio" name="attendanceAmount" value="1" id="val1_<?= $child->getId() ?>"></label>
+				</td>
+				<td>
+					<input type="submit" name="submit" value="Enregistrer" class="attendance">
+				</td>
+			</form>
+		</tr>
 	<?php endforeach; ?>
 </table>
